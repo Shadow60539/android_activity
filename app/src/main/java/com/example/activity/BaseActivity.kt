02ec21c181dd60e3975dfdc1afa1812337b8ab53
Sanjeev Madhav <sduddela@tataunistore.com>
2022-07-14 18:05:59 +0530

    add intent filters

--- app/src/main/java/com/example/activity/BaseActivity.kt
+++ app/src/main/java/com/example/activity/BaseActivity.kt
@@ -5,16 +5,34 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity(private val layOutResId: Int, private val tag: String) :
     AppCompatActivity() {
 
     abstract val context: Context
+    private lateinit var activityResult: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layOutResId)
+        activityResult =
+            registerForActivityResult(
+                ActivityResultContracts.StartActivityForResult(),
+                activityResultCallback(),
+            )
+
+    }
+
+    private fun activityResultCallback() = { result: ActivityResult ->
+        when (result.resultCode) {
+            RESULT_OK -> Toast.makeText(this, result.data?.data?.path, Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 
     override fun onStart() {
@@ -48,8 +66,15 @@ abstract class BaseActivity(private val layOutResId: Int, private val tag: Strin
     }
 
     open fun navigateToSecondActivity(view: View) {
-        val intent = Intent(context, SecondActivity::class.java)
-        startActivity(intent)
+//        val intent = Intent(context, SecondActivity::class.java)
+//        startActivity(intent)
+        val intent = Intent().apply {
+            action = Intent.ACTION_GET_CONTENT
+            type = "image/*"
+//            addCategory(Intent.CATEGORY_DEFAULT)
+        }
+
+        activityResult.launch(intent)
     }
 
     open fun navigateToThirdActivity(view: View) {
